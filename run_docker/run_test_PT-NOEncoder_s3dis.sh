e1bf0ce79a5541ddc961f9d6d74631c8137d9b31
letatanu <letatanu@gmail.com>
2026-06-14 22:08:50 -0400

    update

--- run_docker/run_test_PT-NOEncoder_s3dis.sh
+++ run_docker/run_test_PT-NOEncoder_s3dis.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Set available GPUs (adjust as needed, e.g., "0" or "0,1")
-DEVICES="1,2,3,4,5,6,7"
+DEVICES="7"
 # Calculate number of processes based on devices
 OMP_NUM_THREADS=4
 
@@ -11,11 +11,10 @@ OMP_NUM_THREADS=4
 DOCKER_IMAGE="letatanu/pointcept1"
 
 echo "Starting S3DIS Training on Devices: $DEVICES"
-
 EXP_NAME="semseg-pt-v3-no-encoderonly-v0_02"
-CONFIG_PATH="semseg-pt-v3-no-encoderonly-v0"
 ## --------------------------------------------------------- ##
 DATASET="s3dis"
+echo "Model Name: $MODEL_NAME"
 echo "Devices: $DEVICES"
 
 docker run --ulimit nofile=1048576:1048576 --ipc=host \
@@ -26,8 +25,4 @@ docker run --ulimit nofile=1048576:1048576 --ipc=host \
   -e OMP_NUM_THREADS=${OMP_NUM_THREADS} \
   "${DOCKER_IMAGE}"    \
   bash -lc "
-    sh scripts/train.sh \
-      -p python \
-      -d ${DATASET} \
-      -c ${CONFIG_PATH} \
-      -n ${EXP_NAME}"
+    sh scripts/test.sh -p python -d ${DATASET} -n ${EXP_NAME} -w model_best"
